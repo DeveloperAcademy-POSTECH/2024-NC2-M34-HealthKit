64543d03133dac9ec473c6614d190f9fc7462c6e
DecideJung1221 <jung991221@naver.com>
2024-06-17 13:52:09 +0900

    Initial Commit

---
+++ hitBeat Watch App/hitBeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  hitBeatApp.swift
+//  hitBeat Watch App
+//
+//  Created by 정혜정 on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct hitBeat_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
